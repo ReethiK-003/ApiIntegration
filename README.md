a5f70928063508325745107d357209194921dc17
Manoj <manojagira>
2023-03-28 12:19:35 +0530

    Fixed the swagger implementation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ApiIntegration
 Sute for integration of Thirdy party API services.
+
+#Swagger
+Swagger can run on the http://localhost:9090/swagger-ui/index.html
--- src/main/java/com/apiintegration/core/config/WebSecurityConfig.java
+++ src/main/java/com/apiintegration/core/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -80,6 +81,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		httpSecurity.addFilterAfter(new LogEnhancerFilter(), JwtRequestFilter.class);
+	}
+
+	public void swaggerConfigure(WebSecurity web) throws Exception {
+	    web.ignoring()
+	       .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/webjars/**");
 	}
 
 	@Bean
